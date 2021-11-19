72170dbc917c14f61223144ce7657a2d717598a1
Jason <jason80826@gmail.com>
2021-11-19 17:29:47 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,4 @@ git add -A
 git commit -m 'deploy'
 
 git push -f https://github.com/Jason-lin80826/Vue-movieSearch.git main:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
